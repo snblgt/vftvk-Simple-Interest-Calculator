5ec1de96841bec2e0f8972001f594524c9ffd8f1
snblgt <84819508+snblgt@users.noreply.github.com>
2021-05-28 10:42:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 function compute() {
     
-    // created variables
-
-    var principal= document.getElementById("principal").value;
-
-    var rate= document.getElementById("rate").value;
+    //creating variables
     
-    var years= document.getElementById("years").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;  
+    var years = document.getElementById("years").value; 
+    var interest = principal * years * rate *12/ 100;
     
-    var interest= principal * years * rate / 100;
-    
-    // writed the logic to convert 'no of years' into a year in the future
-
-    var futureyear= parseInt(years) + 2020
-    
-    var result= document.getElementById("result");
-    
-    // text
+    // converting no of years into year in the future
+    var futureyear = parseInt(years) + 2020
+    var result = document.getElementById("result");
 
     if(principal >0){
-    result.innerHTML= "If you deposit <span class='highlight'>"+ principal + "</span> ,<br> at an interest rate of <span class='highlight'>" + rate + "</span>% <br> You will receive an amount of <span class='highlight'>" + interest + "</span>, <br> in the year <span class='highlight'>" + futureyear +"</span>"
+    result.innerHTML = "If you deposit <span class='highlight'>"+ principal + "</span> ,<br> at an interest rate of <span class='highlight'>" + rate + "</span>% <br> You will receive an amount of <span class='highlight'>" + interest + "</span>, <br> in the year <span class='highlight'>" + futureyear +"</span>" 
     }
     else{ 
-
-    // added validation
-
+     
+    // added validation for principal input box
         alert("Please enter a positive number");
         principal.focus();
         return false;
-      } 
     }
+}
+
 function setSliderLabel() {
-    var rate= document.getElementById("rate").value;
-    document.getElementById("rate_span").innerHTML= rate + "%";
- }
+    var rate = document.getElementById("rate").value;
+    document.getElementById("rate_span").innerHTML = rate + "%";
+}    
